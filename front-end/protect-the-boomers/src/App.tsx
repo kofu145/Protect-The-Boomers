81b56fffb0034bf0af3a9e25306a90765dd38c4f
jonathanvdc <jonathan.vdc@outlook.com>
2020-03-18 00:36:21 -0400

    Add login screen

--- front-end/protect-the-boomers/src/App.tsx
+++ front-end/protect-the-boomers/src/App.tsx
@@ -1,45 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
-
-function Index(){
-  return <h2>Home</h2>;
-}
+import Login from './components/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          what is react
-        </p>
-        <p>Oh my god why doesn't this have proper text highlighting</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Login />;
+  
+  // (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         what is react
+  //       </p>
+  //       <p>Oh my god why doesn't this have proper text highlighting</p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 function AppRouter(){
   return (
   <Router>
     <div>
-      <nav>
+      {/* <nav>
         <ul>
           <li>
           <Link to="/">Home</Link>
           </li>
         </ul>
-      </nav>
+      </nav> */}
       <Route path="/" exact component={App} />
     </div>
   </Router>);
